51d355de5c3284ee418f56d03d956a5b077ccc05
OlgaP-1 <72099983+OlgaP-1@users.noreply.github.com>
2021-03-30 21:02:06 +0300

    Update and rename leruaMerlen/spiders/lmru.py to leruaMerlen/Пауков/lmru.py
    add next page for parsing and add comments

--- leruaMerlen/Пауков/lmru.py
+++ leruaMerlen/Пауков/lmru.py
@@ -12,22 +12,16 @@ class LmruSpider(scrapy.Spider):
         self.start_urls = [f'https://leroymerlin.ru/search/?q={search}']
 
     def parse(self, response: HtmlResponse):
-        # На стартовой странице ищем блок с ссылками, за которые можно зацепиться
+      	#находим ссылку на следующую страницу
+        next_page = response.xpath('//a[@class= "paginator-button next-paginator-button"]/@href').extract_first()
+        
         links = response.xpath('//a[contains(@class, "plp-item__info__title")]/@href').extract()
-        # обрабатываем все ссылки
+        
         for link in links:
             yield response.follow(link, callback=self.parse_item)
-        print(links)
-        # Ищем ссылки на следующие страницы
-    # def next_page(self, response: HtmlResponse):
-    #     # paginator = response.xpath('//div[contains(@class, "next-paginator-button-wrapper")]/a/@href')
-    #     # for page_url in paginator: # переходим по этим ссылкам, получая страницы с товаром
-    #     #     yield response.follow(page_url, callback=self.next_page)
-    #     # # Ищем ссылки на товар в открывшейся странице
-    #     href = response.xpath('//uc-product-list/product-card//a[contains(@class, "plp-item__info__title")]/@href').extract()
-    #     for link in href:
-    #         yield response.follow(link, callback=self.parse_item)
-
+        #переходим на следующую страницу и возвращаем метод parse для парсинга новой страницы
+        yield response.follow(next_page, callback=self.parse)
+        
     def parse_item(self, response: HtmlResponse): #обрабатываем каждый товар
         loader = ItemLoader(item=LeruamerlenItem(), response=response)
         loader.add_xpath('photos', '//img[contains(@alt, "product image")]/@src')
@@ -37,9 +31,7 @@ class LmruSpider(scrapy.Spider):
         loader.add_xpath('href', '//uc-regions-overlay-item/a[contains(@class, "region-link highlighted new")]/@href')
         loader.add_xpath('features_keys', '//dt[contains(@class, "def-list__term")]/text()')
         loader.add_xpath('features', '//dd[contains(@class, "def-list__definition")]/text()')
+        # по коду ниже под '#' парсинг не работает по не понятной причине
+        # loader.add_xpath('dic', '//div[contains(@class, "def-list__group")]/text()')
         yield loader.load_item()
-        # item = LeruamerlenItem()
-        # item['name'] = response.xpath('//h1[@itemprop="name"]/text()').extract()
-        # item['photos'] = response.xpath('//img[contains(@alt, "product image")]/@src').extract()
-        # print()
-        # yield item
+
